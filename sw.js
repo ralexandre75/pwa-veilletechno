dd66c61b0b003752aaa21bcef7eaf105a137944a
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-03-15 18:34:04 +0100

    detection of the state of the internet connection and display of an information page

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
 console.log('Hello depuis le service worker');
+
+self.addEventListener('install', evt => {
+    console.log('install evt', evt);
+});
+
+self.addEventListener('activate', evt => {
+    console.log('activate evt', evt);
+});
+
+self.addEventListener('fetch', evt => {
+    if(!navigator.onLine) {
+        const headers = { headers: { 'Content-Type': 'text/html; charset=utf-8'}};
+        evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Application en mode dégradé. Veuillez vous connecter</div>', headers));
+    }
+    console.log('fetch event sur url', evt.request.url);
+});
